32a61793c5f026c0df8b423058ecc66698531410
Ashim Kumar Das <CZ0526@cztech.com>
2026-01-04 16:16:07 +0530

    fixed the template

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from core.app import views
+from app import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
 ]
